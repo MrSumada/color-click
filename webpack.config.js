58cc739b6e42f653eb356ccaf49ac912364b2eee
MrSumada <ricksamericana.com>
2022-06-14 20:02:59 -0400

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src", "index.html"),
+      }),
+    +   require('postcss-100vh-fix'),
+        require('autoprefixer')
+      ],
+};
